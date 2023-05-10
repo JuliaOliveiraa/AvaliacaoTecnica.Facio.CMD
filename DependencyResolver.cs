28a6711eb1f244b41bb8726fdfc73800eb441bc8
Julia Silva de Oliveira <joliveira@bdsdatasolution.com.br>
2023-05-10 14:54:09 -0300

    Adicionar arquivos de projeto.

---
+++ DependencyResolver.cs
@@ -0,0 +1,41 @@
+﻿namespace AvaliacaoTecnica.Facio.CMD;
+
+public class DependencyResolver
+{
+    private Dictionary<string, List<string>> _dependencies;
+
+    public DependencyResolver(Dictionary<string, List<string>> dependencies)
+    {
+        _dependencies = dependencies;
+    }
+
+    public List<string> GetDependencyOrder()
+    {
+        var visited = new List<string>();
+        var order = new List<string>();
+
+        foreach (var dependency in _dependencies.Keys)
+        {
+            if (!visited.Contains(dependency))
+                DFS(dependency, visited, order);
+        }
+
+        return order;
+    }
+
+    private void DFS(string dependency, List<string> visited, List<string> order)
+    {
+        visited.Add(dependency);
+
+        if (_dependencies.ContainsKey(dependency))
+        {
+            foreach (var dependent in _dependencies[dependency])
+            {
+                if (!visited.Contains(dependent))
+                    DFS(dependent, visited, order);
+            }
+        }
+
+        order.Add(dependency);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using AvaliacaoTecnica.Facio.CMD;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dependencies = new Dictionary<string, List<string>>();
+        List<string?> dependenciesList = new List<string?>();
+        try
+        {
+            Console.Write("Quantidade de dependências: ");
+            int numDependencies = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < numDependencies; i++)
+            {
+                Console.Write($"Informe a dependência {i + 1}º: ");
+                dependenciesList.Add(Console.ReadLine());
+            }
+        }
+        catch (Exception e) {
+            Console.WriteLine($"Informações inseridas estão incorretas! \n{e}");
+        }
+
+        foreach (var dependency in dependenciesList)
+        {
+            try
+            {
+                Console.Write($"Quantidade de dependências dependentes de {dependency}: ");
+                int numDependents = int.Parse(Console.ReadLine());
+
+                var dependents = new List<string>();
+
+                for (int j = 0; j < numDependents; j++)
+                {
+                    Console.Write($"Informe a dependência dependente {j + 1} de {dependency}: ");
+                    string dependent = Console.ReadLine();
+                    dependents.Add(dependent);
+                }
+
+                dependencies.Add(dependency, dependents);
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Informações inseridas estão incorretas! \n{e}");
+                return;
+            }
+        }
+
+        try
+        {
+            var resolver = new DependencyResolver(dependencies);
+            var order = resolver.GetDependencyOrder();
+            Console.WriteLine("Ordem de instalação das Dependências:");
+            foreach (var dependency in order)
+            {
+                Console.WriteLine(dependency);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
